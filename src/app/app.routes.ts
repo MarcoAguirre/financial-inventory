ddba66e6437e6f82616c6ea6c5eca9efce841c23
MarcoAguirre <viniciosoria16@gmail.com>
2025-07-18 17:46:50 -0500

    feat: Add a route to access the new created component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { ProductPageComponent } from './components/product-page/product-page.component';
+import { ProductFormComponent } from './components/product-form/product-form.component';
 
 export const routes: Routes = [
-    {
-    path: '',
-    component: ProductPageComponent
-  }
+  { path: '', component: ProductPageComponent },
+  { path: 'create', component: ProductFormComponent },
 ];
